b6c12d02fce255fe4fd2865f86c2efdb1c454845
Mandalorian83 <83162001+Mandalorian83@users.noreply.github.com>
2021-09-08 21:32:31 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{fabric.Image.fromURL("BirthdayImage.png" , function(img){
+    block_img_object = img;
+    block_img_object.scaleToWidth(150);
+    block_img_object.scaleToHeight(140);
+    block_img_object.set
+	
+	
+}
+
+function playSound(){
+	x.play("bdayaudio.mp3")
+}
